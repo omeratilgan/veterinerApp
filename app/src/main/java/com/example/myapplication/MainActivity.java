b0a74d0dbdccbb371969ad68f9bfc04e68667e4b
Ömer Atılgan <omeratilgan26@gmail.com>
2024-07-17 20:39:45 +0300

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,11 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        deneme();
+        calculator();
     }
 
 
     public void deneme(){
         int x = 7;
         System.out.println(x);
+    }
+
+    public void calculator(){
+        int a = 5;
+        int b = 4;
+        System.out.println(a+b);
     }
 }
