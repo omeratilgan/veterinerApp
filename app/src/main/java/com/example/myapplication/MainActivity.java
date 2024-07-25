ce0098d0d963242c83bc5670fc8d89d04f6f8a8c
Ömer Atılgan <omeratilgan26@gmail.com>
2024-07-25 03:28:51 +0300

    login and signin screen ui finish

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void girisYap(View view){
+        Intent giris = new Intent(this, loginScreen.class);
+        startActivity(giris);
+        finish();
+    }
+
+    public void kayitOl(View view){
+        Intent giris = new Intent(this, signinScreen.class);
+        startActivity(giris);
+        finish();
     }
 
 }
